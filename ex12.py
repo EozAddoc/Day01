0fa60399a6225be58deb0eee4ee7b26146b1cfae
User <user@Eozbs-MacBook-Pro.local>
2025-01-06 12:50:44 +0100

    added comments

--- ex12.py
+++ ex12.py
@@ -8,12 +8,13 @@ import random
 """
 
 def generateMDP():
+    # chat a fais les liste pour moi mais jai fais la logique
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     special_characters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
     numbers = [str(i) for i in range(10)]
     length = int(input("how long would you like your password to be ?"))
     a = random.randint(1, length-2)
-    b = random.randint(1, length-a-1)  # Ensure the remaining space is enough for 'b'
+    b = random.randint(1, length-a-1)  
     c = length - a - b 
     rand_lett= random.sample(letters,a)
     rand_sp=random.sample(special_characters,b)
